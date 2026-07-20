e823374addc567c1622c142a042c5924187dc0a1
shreya300907 <kumarishreya300907@gmail.com>
2026-07-20 11:29:07 +0530

    fix:theme toggle

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+  reactCompiler: false,
   typescript: {
     ignoreBuildErrors: true,
   },
